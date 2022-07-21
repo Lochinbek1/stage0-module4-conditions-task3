ca7b68cb03b718c4071fbc4e474c774f8e3d8061
Qodirov Lochinbek <lochinbek433@gmail.com>
2022-07-21 20:31:45 +0500

    Done!

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        float tax;
+        if (salary > 0 && salary < 10000) {
+            tax = (float)(salary - (salary * 0.15));
+            System.out.println(tax);
+        } else if (salary > 10000 && salary < 20000) {
+            tax = (float)(salary - (salary * 0.18));
+            System.out.println(tax);
+        } else if(salary > 20000){
+            tax = (float)(salary - (salary * 0.20));
+            System.out.println(tax);
+        } else{
+            System.out.println("wrong input!");
+        }
     }
 }
